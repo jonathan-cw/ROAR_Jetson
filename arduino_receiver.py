b60432cdf6c926607682119a88c91b5a02d8dce9
Jonathan Wong <jonathancw@berkeley.edu>
2020-12-08 17:11:14 -0800

    Update arduino_receiver.py

--- arduino_receiver.py
+++ arduino_receiver.py
@@ -19,10 +19,10 @@ UDP_PORT = 7788
 class ArduinoReceiver:
     def __init__(self, client_ip: str, serial: Optional[Serial] = None):
         self.serial = serial if serial is not None else self._create_serial()
-        self.old_steering = 0.0
-        self.old_throttle = 0.0
-        self.new_steering = 0.0
-        self.new_throttle = 0.0
+        self.fl = 0
+        self.fr = 0
+        self.bl = 0
+        self.br = 0
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.client_ip = client_ip
         self.logger = logging.getLogger("Arduino Receiver")
@@ -31,9 +31,9 @@ class ArduinoReceiver:
     @staticmethod
     def _create_serial():
         if 'win' in sys.platform:
-            serial = Serial(port='COM4', baudrate=9600, timeout=1, writeTimeout=1)
+            serial = Serial(port='COM5', baudrate=115200, timeout=1, writeTimeout=1)
         else:
-            serial = Serial(port='/dev/ttyACM0', baudrate=9600, timeout=1, writeTimeout=1)
+            serial = Serial(port='/dev/ttyACM0', baudrate=115200, timeout=1, writeTimeout=1)
         return serial
 
     def update(self):
@@ -68,7 +68,7 @@ class ArduinoReceiver:
     def run_threaded(self, **args):
         try:
             vel_wheel = self.serial.readline().decode().rstrip()
-            print(vel_wheel)
+            self.fl, self.fr, self.bl, self.br = vel_wheel.split(" , ")
         except Exception as e:
             print(e)
 
@@ -76,8 +76,8 @@ class ArduinoReceiver:
 if __name__ == '__main__':
     import time
 
-    serial_connection = Serial("PORT ADDRESS HERE", baudrate=9600, timeout=0.5, writeTimeout=0.5)
+    serial_connection = Serial("COM5", baudrate=115200, timeout=0.5, writeTimeout=0.5)
     arduino_cmd_receiver = ArduinoReceiver(client_ip="localhost", serial=serial_connection)
-    for i in range(10):
+    while True:
         arduino_cmd_receiver.run_threaded()
-        time.sleep(1)  # you cant send too fast!
+        print(arduino_cmd_receiver.fl, arduino_cmd_receiver.fr, arduino_cmd_receiver.bl, arduino_cmd_receiver.br)
